9a710103fe700417253b09816ec0e96637ae012f
mona <mona.sharma14998@gmail.com>
2022-04-07 17:39:56 +0530

    first commit

---
+++ src/LargestNumber/LargestNum.java
@@ -0,0 +1,27 @@
+package LargestNumber;
+
+public class LargestNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=100;
+		int b=20;
+		int c=50;
+		if(a>b && a>c)
+		{
+			System.out.println(a);
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println(b);
+			
+		}
+		else
+		{
+			System.out.println(c);
+		}
+
+	}
+
+}
